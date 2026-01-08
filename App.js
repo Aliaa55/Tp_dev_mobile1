da25052a552a27f9979a0c948470abe6a070a9ce
Aliaa55 <aliaachibel8@gmail.com>
2026-01-08 17:19:22 +0100

    TP7

--- App.js
+++ App.js
@@ -1,79 +1,48 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import SettingsScreen from './screens/SettingsScreen';
-import HomeStack from './screens/HomeStack';
-import AppBar from './screens/AppBar';
-import { Ionicons } from '@expo/vector-icons';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'; 
-import AuthProvider, { AuthContext } from "./context/AuthContext";
-import AppDrawer from "./navigation/AppDrawer";
-import LoginScreen from "./screens/LoginScreen";
-import { useContext } from "react";
-import { Provider } from "react-redux";
-import { store } from "./store/store";
-
-
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-
-function RootNavigator() {
-  const { user } = useContext(AuthContext);
-  return user ? <AppDrawer /> : <LoginScreen />;
-}
-
-
-function HomeTabNavigator() {
+import { useEffect, useState, useContext } from "react";
+import { View, StyleSheet, ActivityIndicator } from "react-native";
+import { initDB } from "./services/database";
+import { ThemeProvider, ThemeContext } from "./context/ThemeContext";
+import TodoListOfflineScreen from "./screens/TodoListOfflineScreen";
+function MainApp() {
+  const { theme } = useContext(ThemeContext);
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
-      <AppBar />
-      <Tab.Navigator
-        screenOptions={{
-          headerShown: false,
-          tabBarActiveTintColor: 'blue',
-          tabBarInactiveTintColor: 'gray',
-          tabBarStyle: { 
-            backgroundColor: '#f0f0f0'
-          },
-          tabBarLabelStyle: { 
-            fontSize: 14
-          },
-        }}
-      >
-        <Tab.Screen 
-          name="Maison" 
-          component={HomeStack} 
-          options={{ 
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="home" size={size} color={color} />
-            ),
-          }} 
-        />
-        <Tab.Screen 
-          name="Paramètres" 
-          component={SettingsScreen} 
-          options={{ 
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="settings" size={size} color={color} />
-            ),
-          }} 
-        />
-      </Tab.Navigator>
-    </SafeAreaView>
+    <View
+      style={[
+        styles.container,
+        theme === "dark" ? styles.dark : styles.light,
+      ]}
+    >
+      <TodoListOfflineScreen />
+    </View>
   );
 }
-
 export default function App() {
-  return (
-    <Provider store={store}>
-      <AuthProvider>
-        <SafeAreaProvider>
-          <NavigationContainer>
-            <RootNavigator />
-          </NavigationContainer>
-        </SafeAreaProvider>
-      </AuthProvider>
-    </Provider>
-  );
+const [dbReady, setDbReady] = useState(false);
+useEffect(() => {
+ const prepareDb = async () => {
+ await initDB(); // attendre SQLite
+ setDbReady(true); // OK pour afficher l’app
+ };
+ prepareDb();
+}, []);
+if (!dbReady) {
+ return <ActivityIndicator size="large" />;
 }
+return (
+ <ThemeProvider>
+ <MainApp />
+ </ThemeProvider>
+);
+}
+const styles = StyleSheet.create({
+container: {
+ flex: 1,
+ paddingTop: 40,
+},
+light: {
+ backgroundColor: "#ffffff",
+},dark: {
+ backgroundColor: "#121212",
+},
+});
+
